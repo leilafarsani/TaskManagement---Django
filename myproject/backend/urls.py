a99220734b503db30222fc63df3b2419f17dc994
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2024-01-27 23:40:16 +0000

    set Up URL Patterns

--- myproject/backend/urls.py
+++ myproject/backend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import UserListView
+from . import views
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user_list'),
-
+    path('tasks/', views.task_list, name='task_list'),
+    # I will add other paths for creating, updating, and deleting tasks, and etc later
 ]
-
+   
